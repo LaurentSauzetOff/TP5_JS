15758ec24e953db44bfcc395e4666ce132c6ae23
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2023-08-05 10:24:42 +0200

    Recipe Schema

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,3 +1,4 @@
+/* const Category = require("../models/Category.js"); */
 const Category = require("../models/Category");
 const Recipe = require("../models/Recipe");
 
@@ -47,4 +48,37 @@ exports.exploreCategories = async (req, res) => {
   }
 };
 
+async function insertDummyRecipeData() {
+  try {
+    await Recipe.insertMany([
+      {
+        name: "Recipe Name Goes Here",
+        description: `Recipe Description Goes Here`,
+        email: "recipeemail@raddy.co.uk",
+        ingredients: [
+          "1 level teaspoon baking powder",
+          "1 level teaspoon cayenne pepper",
+          "1 level teaspoon hot smoked paprika",
+        ],
+        category: "American",
+        image: "southern-friend-chicken.jpg",
+      },
+      {
+        name: "Recipe Name Goes Here",
+        description: `Recipe Description Goes Here`,
+        email: "recipeemail@raddy.co.uk",
+        ingredients: [
+          "1 level teaspoon baking powder",
+          "1 level teaspoon cayenne pepper",
+          "1 level teaspoon hot smoked paprika",
+        ],
+        category: "American",
+        image: "southern-friend-chicken.jpg",
+      },
+    ]);
+  } catch (error) {
+    console.log("err", +error);
+  }
+}
 
+insertDummyRecipeData();
--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,35 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const recipeSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: 'Ce champ est requis.'
+    required: "Ce champ est requis.",
   },
   description: {
     type: String,
-    required: 'Ce champ est requis.'
+    required: "Ce champ est requis.",
   },
   email: {
     type: String,
-    required: 'Ce champ est requis.'
+    required: "Ce champ est requis.",
   },
   ingredients: {
     type: Array,
-    required: 'Ce champ est requis.'
+    required: "Ce champ est requis.",
   },
   category: {
     type: String,
-    enum: ['Thai', 'Américain', 'Chinois', 'Mexicain', 'Indien'],
-    required: 'Ce champ est requis.'
+    enum: ["Thai", "Américain", "Chinois", "Mexicain", "Indien"],
+    required: "Ce champ est requis.",
   },
   image: {
     type: String,
-    required: 'Ce champ est requis.'
+    required: "Ce champ est requis.",
   },
 });
 
-recipeSchema.index({ name: 'text', description: 'text' });
-// WildCard Indexing
-//recipeSchema.index({ "$**" : 'text' });
+recipeSchema.index({ name: "text", description: "text" });
 
-module.exports = mongoose.model('Recipe', recipeSchema);
+module.exports = mongoose.model("Recipe", recipeSchema);
